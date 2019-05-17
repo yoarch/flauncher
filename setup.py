70e83abd630d57b4704a865d9544890dfc9d7a85
yo <yo.managements@gmail.com>
2019-05-17 08:23:39 +0200

    improvements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flauncher",
-    version="1.1.4",
+    version="1.1.5",
     python_requires='>=3',
     author="yoarch",
     author_email="yo.managements@gmail.com",
